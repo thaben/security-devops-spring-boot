f3d64736b8356677f4d44fecaf695a9a728001a0
ben <noemail@ben.com>
2020-12-29 20:36:36 +0200

    fixes

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,10 +1,11 @@
 package com.example.demo.controllers;
 
-import java.util.Optional;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,33 +22,65 @@ import com.example.demo.model.requests.CreateUserRequest;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private CartRepository cartRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@GetMapping("/id/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		return ResponseEntity.of(userRepository.findById(id));
 	}
-	
+
 	@GetMapping("/{username}")
 	public ResponseEntity<User> findByUserName(@PathVariable String username) {
-		User user = userRepository.findByUsername(username);
+		User user = userRepository.findByUsername(username).orElse(null);
 		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
 	}
-	
+
 	@PostMapping("/create")
-	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+	public ResponseEntity<?> createUser(@RequestBody CreateUserRequest createUserRequest) {
+
+		LOGGER.info("CreateUserRequest {} ", createUserRequest);
+
+		if(createUserRequest.getPassword().length() < 8){
+			LOGGER.error("User not created because Password cannot be less than 8 characters");
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Password cannot be less than 8 characters");
+		}
+
+		if( !createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
+			LOGGER.error("User not created because Passwords are not the same");
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Passwords are not the same");
+		}
+
+		if (createUserRequest.getUsername() == null || createUserRequest.getUsername().isEmpty()) {
+			LOGGER.info("Username {} cannot be null of empty", createUserRequest.getUsername());
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+		}
+
+		if( userRepository.findByUsername(createUserRequest.getUsername()).isPresent()){
+			LOGGER.error("Username {} already exists", createUserRequest.getUsername());
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+		}
+
+
 		User user = new User();
 		user.setUsername(createUserRequest.getUsername());
+		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
+		LOGGER.info("Encoded password is {}",user.getPassword());
 		Cart cart = new Cart();
 		cartRepository.save(cart);
 		user.setCart(cart);
-		userRepository.save(user);
+		User userSaved = userRepository.save(user);
+		LOGGER.info("User Created {} ", userSaved);
+
 		return ResponseEntity.ok(user);
 	}
-	
+
 }
